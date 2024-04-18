010ad947eb1b83dad973f0b7573875667197b184
ShawnaSong <xinyis10@illinois.edu>
2024-04-18 12:57:32 +0800

    picture renamed

---
+++ rename.py
@@ -0,0 +1,14 @@
+import os
+
+# path_name = 'pomfred'
+# i = 0
+# for item in os.listdir(path_name):
+#     os.rename(os.path.join(path_name, item), os.path.join(path_name, 'pomfred'+str(i)+'.png'))
+#     i += 1
+
+
+path_name = 'silver carp'
+i = 0
+for item in os.listdir(path_name):
+    os.rename(os.path.join(path_name, item), os.path.join(path_name, 'silver_carp'+str(i)+'.png'))
+    i += 1
