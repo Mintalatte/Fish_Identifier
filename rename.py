b25ae701b9bdf314f73f145314abbdcdbac8c4f0
ShawnaSong <xinyis10@illinois.edu>
2024-04-23 11:38:07 +0800

    Silver Carp picture added

--- rename.py
+++ rename.py
@@ -7,14 +7,14 @@ import os
 #     i += 1
 
 
-# path_name = 'silver carp'
-# i = 0
-# for item in os.listdir(path_name):
-#     os.rename(os.path.join(path_name, item), os.path.join(path_name, 'silver_carp'+str(i)+'.png'))
-#     i += 1
-
-path_name = 'Pomfret'
+path_name = 'SilverCarp'
 i = 0
 for item in os.listdir(path_name):
-    os.rename(os.path.join(path_name, item), os.path.join(path_name, 'pomfret'+str(i)+'.png'))
+    os.rename(os.path.join(path_name, item), os.path.join(path_name, 'silver_carp'+str(i)+'.png'))
     i += 1
+
+# path_name = 'Pomfret'
+# i = 0
+# for item in os.listdir(path_name):
+#     os.rename(os.path.join(path_name, item), os.path.join(path_name, 'pomfret'+str(i)+'.png'))
+#     i += 1
